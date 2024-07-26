9ab5a7c4eb8bfd2c4f30882d931a09db072e69e6
shourya mishra <shourya.desk@gmail.com>
2024-07-26 16:10:16 +0530

    done

---
+++ app/_document.js
@@ -0,0 +1,28 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Google Tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QNTXFYSL82"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QNTXFYSL82');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -16,8 +16,6 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Pathway+Gothic+One&display=swap" rel="stylesheet"></link>
-        
-
       </head>
       <body className={inter.className}>
         {children}
