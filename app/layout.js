85b3dcd890f70234ebfa80222573ec76b29898d4
shourya mishra <shourya.desk@gmail.com>
2024-07-26 22:47:32 +0530

    gtag

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M62PZ53J');
+          `}
+        </script>
         <link rel="icon" href="/favicon.png" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -33,7 +43,18 @@ export default function RootLayout({ children }) {
           `}
         </script>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M62PZ53J"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   );
 }
